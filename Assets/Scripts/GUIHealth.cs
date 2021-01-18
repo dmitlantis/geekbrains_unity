296b7bc0197c84c0884617aff0f558c156f7f684
dmitlantis <dmitlantis@gmail.com>
2021-01-18 17:18:54 +0300

    Lesson 2.2

---
+++ Assets/Scripts/GUIHealth.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIHealth : MonoBehaviour
+{
+    [SerializeField] public LivesMeter livesMeter;
+    
+    private void OnGUI()
+    {
+        GUI.Box(new Rect(10,150,50,50), "Health");
+        GUI.Label(new Rect(15, 180, 40, 40), livesMeter.health.ToString());
+    }
+}
